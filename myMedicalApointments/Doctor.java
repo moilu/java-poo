25d1a73ea31d8c1585424cddaba12430d917fbc2
Moises <moisesluna0390@gmail.com>
2021-09-17 12:43:16 -0700

    finished the java Doctor class

---
+++ myMedicalApointments/Doctor.java
@@ -0,0 +1,12 @@
+package myMedicalApointments;
+public class Doctor {
+    //Atributos
+    int id;
+    String name;
+    String speciality;
+
+    //Metodos
+    public void showName() {
+        System.out.println(name);
+    }
+}
---
+++ myMedicalApointments/Main.java
@@ -0,0 +1,9 @@
+package myMedicalApointments;
+
+public class Main {
+    public static void main(String[] args) {
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Moises Luna";
+        myDoctor.showName();
+    }
+}
